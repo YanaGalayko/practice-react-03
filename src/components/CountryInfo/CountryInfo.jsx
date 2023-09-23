64a767c1793ee2e236ba9bf4df72855e4524ccd4
bakkara <ane4karymar@gmail.com>
2023-09-23 12:29:04 +0300

    add country info

--- src/components/CountryInfo/CountryInfo.jsx
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -9,35 +9,33 @@ import {
   Accent,
 } from './CountryInfo.styled';
 
-export const CountryInfo = ({
+export const CountryInfo = ({country: {
   flag,
   capital,
-  country,
-  id,
+  countryName,
   languages = [],
   population,
-}) => {
+}}) => {
   return (
-    <h2>CountryInfo</h2>
-    // <CountryWrapper >
-    //   <Flag>
-    //     <Image />
-    //   </Flag>
-    //   <CountryDescription>
-    //     <CountryCapital>
-    //       Capital: <Accent></Accent>
-    //     </CountryCapital>
+    <CountryWrapper >
+    <Flag>
+        <Image src={flag} alt={countryName}/>
+      </Flag>
+      <CountryDescription>
+        <CountryCapital>
+          Capital: <Accent>{ capital }</Accent>
+        </CountryCapital>
 
-    //     <CountryTitle></CountryTitle>
+        <CountryTitle>{countryName === 'Russian Federation' ? 'MORDOR' : countryName}</CountryTitle>
 
-    //     <CountryDetail>
-    //       Population: <Accent></Accent>
-    //     </CountryDetail>
+        <CountryDetail>
+          Population: <Accent>{population}</Accent>
+        </CountryDetail>
 
-    //     <CountryDetail>
-    //       Languages: <Accent></Accent>
-    //     </CountryDetail>
-    //   </CountryDescription>
-    // </CountryWrapper>
+        <CountryDetail>
+          Languages: <Accent>{ languages.join(', ')}.</Accent>
+        </CountryDetail>
+      </CountryDescription>
+    </CountryWrapper>
   );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,17 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useFetchCountry } from 'hooks';
+import { Navigate } from 'react-router-dom';
+import { routes } from 'routes';
 
 export const Country = () => {
+  const { country, loading, error } = useFetchCountry()
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        {error && <Navigate to={ routes.HOME }/>}
+        {loading && <Loader />}
+        {country && <CountryInfo country={ country } />}
       </Container>
     </Section>
   );
